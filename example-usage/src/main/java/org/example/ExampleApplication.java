1cd3c722671bb22e29c0c8f9729d92eb458a2a9d
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-10-17 11:50:30 +0200

    feat(api): replace @EnableDkronScheduler with defining auto configurations in META-INF

--- example-usage/src/main/java/org/example/ExampleApplication.java
+++ example-usage/src/main/java/org/example/ExampleApplication.java
@@ -1,14 +1,9 @@
 package org.example;
 
-import com.codibly.schedulerclient.EnableDkronScheduler;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// To enabled scheduler add @EnableDkronScheduler annotation
-@EnableDkronScheduler
 public class ExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
