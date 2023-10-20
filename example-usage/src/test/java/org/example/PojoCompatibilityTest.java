e992ee7cbe7f11586d446e3546410d780fca4440
Tomasz Orchowski <tomasz.orchowski@codibly.com>
2023-10-20 18:11:19 +0200

    api(test-tools): add test tool subproject

---
+++ example-usage/src/test/java/org/example/PojoCompatibilityTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.codibly.schedulerclient.JobNotificationPayloadCompatibilityChecker;
+import org.junit.jupiter.api.Test;
+
+class PojoCompatibilityTest {
+
+
+    // Create test for checking if given pojo can be used as a job payload
+    @Test
+    void  checkIfPojoIsCompatibleWithDkronScheduler(){
+        // Given
+        PrintSomethingJobDto helloWorld = new PrintSomethingJobDto("Hello World");
+
+        // Then
+        JobNotificationPayloadCompatibilityChecker.of(helloWorld).verifySerializationCompatibility();
+    }
+}
